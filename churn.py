523e6f915cf8dbeb13987db669c5ea130bbb3739
QomariyahJunaidi <107415647+QomariyahJunaidi@users.noreply.github.com>
2022-08-06 17:51:34 +0800

    Update churn.py

--- churn.py
+++ churn.py
@@ -1,234 +1,79 @@
-import streamlit as st 
-import numpy as np 
+import streamlit as st
 import pandas as pd
-
-#import matplotlib.pyplot as plt
-from matplotlib import pyplot as plt
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
-
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-from sklearn.metrics import classification_report
-
-
-
-st.title('Machine Learning - CLASSIFICATION')
-
-st.sidebar.write("""
-This is a web app demo using python libraries such as Streamlit, Sklearn etc
+st.write("""
+# Simple Prediction for Churn Activity App
+This app predicts the **Customer Churn**!
 """)
 
-st.sidebar.write ("For more info, please contact:")
+st.sidebar.header('User Input Parameters')
 
-st.sidebar.write("<a href='https://www.linkedin.com/in/qomariyah-junaidi/'>Qomariyah Junaidi</a>", unsafe_allow_html=True)
-
-
-choice = st.sidebar.radio(
-    "Choose a dataset",   
-    ('Default', 'User-defined '),
-    index = 0
+def user_input_features():
+    st = st.sidebar.slider('st', 0.0, 50.0, 20.0)
+    acclen = st.sidebar.slider('acclen', 1.0, 243.0, 100.0)
+    arcode = st.sidebar.slider('arcode', 408.0, 510.0, 100.0)
+    intplan = st.sidebar.slider('intplan', 0.0, 1.0, 0.0)
+    voice = st.sidebar.slider('voice', 0.0, 1.0, 0.0)
+    nummailmes = st.sidebar.slider('intplan', 0.0, 51.0, 0.0)
+    tdmin = st.sidebar.slider('tdmin', 0.0, 350.8, 100.0)
+    tdcal = st.sidebar.slider('tdcal', 0.0, 165.0, 50.0)
+    tdchar = st.sidebar.slider('tdchar', 0.0, 59.64, 10.0)
+    temin = st.sidebar.slider('temin', 0.0, 363.7, 100.0)
+    tecal = st.sidebar.slider('tecal', 0.0, 363.7, 100.0)
+    tecahr = st.sidebar.slider('tecal', 0.0, 30.91, 10.0)
+    tnmin = st.sidebar.slider('tnmin', 23.2, 395.0, 100.0)
+    tn.cal = st.sidebar.slider('tn.cal', 33.0, 175, 100.0)
+    tnchar = st.sidebar.slider('tnchar', 1.04, 17.77, 5.0)
+    timin = st.sidebar.slider('timin', 0.0, 20.0, 5.0)
+    tical = st.sidebar.slider('tecal', 0.0, 363.7, 100.0)
+    tichar = st.sidebar.slider('tichar', 0.0, 5.4, 1.0)
+    ncsc = st.sidebar.slider('ncsc', 0.0, 9.0, 1.0)
     
-)
+    data = {'st': st,
+            'acclen': acclen,
+            ' arcode':  arcode,
+            'intplan': intplan,
+             'voice' : voice,
+            'nummailmes' :  nummailmes,
+            'tdmin' : tdmin,
+            'tdcal' : tdcal,
+            'tdchar' : tdchar,
+            'temin' : temin,
+            'tecal' : tecal,
+            'tecahr' : tecahr,
+            'tnmin' : tnmin,
+            'tn.cal' : tn.cal,
+            'tnchar' : tnchar,
+            'timin' : timin,
+            'tical' : tical,
+            'tichar' : tichar,
+            'ncsc' : ncsc}
+    features = pd.DataFrame(data, index=[0])
+    return features
 
-st.write(f"## You Have Selected <font color='Aquamarine'>{choice}</font> Dataset", unsafe_allow_html=True)
+df = user_input_features()
 
-def get_default_dataset(name):
-    data = None
-    if name == 'Iris':
-        data = datasets.load_iris()
-    elif name == 'Wine':
-        data = datasets.load_wine()
-    else:
-        data = datasets.load_breast_cancer()
-    X = data.data
-    y = data.target
-    return X, y
+st.subheader('User Input parameters')
+st.write(df)
 
-def add_dataset_ui(choice_name):
-    X=[]
-    y=[]
-    X_names = []
-    X1 = []
-    if choice_name == 'Default':
-       dataset_name = st.sidebar.selectbox(
-            'Select Dataset',
-            ('Iris', 'Breast Cancer', 'Wine')
-        )
-       X, y = get_default_dataset (dataset_name)
-       X_names = X
-    else:
-        uploaded_file = st.sidebar.file_uploader(
-            "Upload a CSV",
-            type='csv'    )
-        
+churn = datasets.load_churn()
+X = churn.data
+Y = churn.target
 
-        if uploaded_file!=None:
-           
-           st.write(uploaded_file)
-           data = pd.read_csv(uploaded_file)
-  
-        
-           y_name = st.sidebar.selectbox(
-                    'Select Label @ y variable',
-                    sorted(data)
-                    )
+clf = RandomForestClassifier()
+clf.fit(X, Y)
 
-           X_names = st.sidebar.multiselect(
-                     'Select Predictors @ X variables.',
-                     sorted(data),
-                     default = sorted(data)[1],
-                     help = "You may select more than one predictor"
-                     )
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
 
-           y = data.loc[:,y_name]
-           X = data.loc[:,X_names]
-           X1 = X.select_dtypes(include=['object'])
-        
-           X2 = X.select_dtypes(exclude=['object'])
+st.subheader('Churn labels and their corresponding index number')
+st.write(churn.target_names)
 
-           if sorted(X1) != []:
-              X1 = X1.apply(LabelEncoder().fit_transform)
-              X = pd.concat([X2,X1],axis=1)
+st.subheader('Prediction')
+st.write(churn.target_names[prediction])
+#st.write(prediction)
 
-           y = LabelEncoder().fit_transform(y)
-        else:
-           st.write(f"## <font color='Aquamarine'>Note: Please upload a CSV file to analyze the data.</font>", unsafe_allow_html=True)
-
-    return X,y, X_names, X1
-
-X, y , X_names, cat_var= add_dataset_ui (choice)
-
-
-
-
-classifier_name = st.sidebar.selectbox(
-    'Select classifier',
-    ('KNN', 'SVM', 'Random Forest')
-)
-
-test_data_ratio = st.sidebar.slider('Select testing size or ratio', 
-                                    min_value= 0.10, 
-                                    max_value = 0.50,
-                                    value=0.2)
-random_state = st.sidebar.slider('Select random state', 1, 9999,value=1234)
-
-st.write("## 1: Summary (X variables)")
-
-
-if len(X)==0:
-   st.write("<font color='Aquamarine'>Note: Predictors @ X variables have not been selected.</font>", unsafe_allow_html=True)
-else:
-   st.write('Shape of predictors @ X variables :', X.shape)
-   st.write('Summary of predictors @ X variables:', pd.DataFrame(X).describe())
-
-st.write("## 2: Summary (y variable)")
-
-if len(y)==0:
-   st.write("<font color='Aquamarine'>Note: Label @ y variable has not been selected.</font>", unsafe_allow_html=True)
-elif len(np.unique(y)) <5:
-     st.write('Number of classes:', len(np.unique(y)))
-
-else: 
-   st.write("<font color='red'>Warning: System detects an unusual number of unique classes. Please make sure that the label @ y is a categorical variable. Ignore this warning message if you are sure that the y is a categorical variable.</font>", unsafe_allow_html=True)
-   st.write('Number of classes:', len(np.unique(y)))
-
-
-def add_parameter_ui(clf_name):
-    params = dict()
-    if clf_name == 'SVM':
-        C = st.sidebar.slider('C', 0.01, 10.0,value=1.0)
-        params['C'] = C
-    elif clf_name == 'KNN':
-        K = st.sidebar.slider('K', 1, 15,value=5)
-        params['K'] = K
-    else:
-        max_depth = st.sidebar.slider('max_depth', 2, 15,value=5)
-        params['max_depth'] = max_depth
-        n_estimators = st.sidebar.slider('n_estimators', 1, 100,value=10)
-        params['n_estimators'] = n_estimators
-    return params
-
-params = add_parameter_ui(classifier_name)
-
-def get_classifier(clf_name, params):
-    clf = None
-    if clf_name == 'SVM':
-        clf = SVC(C=params['C'])
-    elif clf_name == 'KNN':
-        clf = KNeighborsClassifier(n_neighbors=params['K'])
-    else:
-        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'], 
-            max_depth=params['max_depth'], random_state=random_state)
-    return clf
-
-clf = get_classifier(classifier_name, params)
-
-
-st.write("## 3: Classification Report")
-
-if len(X)!=0 and len(y)!=0: 
-
-
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_data_ratio, random_state=random_state)
-
-  scaler = StandardScaler()
-  X_train_scaled = scaler.fit_transform(X_train)
-  X_test_scaled = scaler.transform(X_test)    
-
-  clf.fit(X_train_scaled, y_train)
-  y_pred = clf.predict(X_test_scaled)
-
-
-  st.write('Classifier:',classifier_name)
-  st.write('Classification report:')
-  report = classification_report(y_test, y_pred,output_dict=True)
-  df = pd.DataFrame(report).transpose()
-  st.write(df)
-
-else: 
-   st.write("<font color='Aquamarine'>Note: No classification report generated.</font>", unsafe_allow_html=True)
-
-
-st.write("## 4: Principal Component Analysis Plot")
-suitable = 1
-if len(X_names) <2:
-  st.write("<font color='Aquamarine'>Note: No PCA plot as it requires at least two predictors.</font>", unsafe_allow_html=True)
-  suitable = 0
-else:
-    for names in X_names:
-        if names in cat_var:
-           st.write("<font color='Aquamarine'>Note: No PCA plot as it only supports numerical predictors.</font>", unsafe_allow_html=True)
-           suitable = 0
-           break
-
-if suitable == 1:
-   pca = PCA(2)
-   X_projected = pca.fit_transform(X)
-
-   x1 = X_projected[:, 0]
-   x2 = X_projected[:, 1]
-
-   fig = plt.figure()
-   plt.scatter(x1, x2,
-               c=y, alpha=0.8,
-               cmap='viridis')
-
-   plt.xlabel('Principal Component 1')
-   plt.ylabel('Principal Component 2')
-   plt.colorbar()
-   st.pyplot(fig)
-            
-            
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
